b0389f931d88159eb678e2f7d642c3a210aff979
sachinkn92 <130657745+sachinkn92@users.noreply.github.com>
2025-12-23 23:11:48 +0530

    Clean up truffle-config.js by removing comments
    Removed commented documentation for network configuration.

--- truffle-config.js
+++ truffle-config.js
@@ -24,15 +24,7 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
+  
 
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
